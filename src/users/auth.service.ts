29a6da72a808791c748a2a27eaf846228da91182
adisaktijrs <adhisakti47@gmail.com>
2023-06-18 23:30:30 +0700

    feat: add sigin

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
@@ -30,5 +35,21 @@ export class AuthService {
     return this.usersService.create(email, result);
   }
 
-  signin() {}
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new UnauthorizedException('bad password');
+    }
+
+    return user;
+  }
 }
